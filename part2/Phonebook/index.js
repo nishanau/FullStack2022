586acda2c17ab84517255a9aa9e2ace4adfa90eb
nishanau <110711217+nishanau@users.noreply.github.com>
2022-09-10 19:43:59 +0545

    Create index.js

---
+++ part2/Phonebook/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+//import App from './Phonebook'
+import App from './Phonebook'
+//import App from './App'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
